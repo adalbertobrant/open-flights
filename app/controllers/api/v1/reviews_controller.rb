50ada29285d882d252e0e6b274fbef6b1c426359
adalbertobrant <adalbertobrant@gmail.com>
2021-02-28 09:52:52 -0300

    WIP part 3 - building react front page using api in ruby

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -4,7 +4,7 @@ module Api
             protect_from_forgery with: :null_session
             
             def create
-                review = Review.new(review_params)
+                review = airline.reviews.new(review_params)
 
                 if review.save
                     render json: ReviewSerializer.new(review).serialized_json
@@ -22,7 +22,12 @@ module Api
                     render json: {error: review.errors.messages}, status: 442
                 end
             end
+
             private
+
+            def airline
+                @airline ||= Airline.find(params[:airline_id])
+            end
 
             def review_params
                 params.require(:review).permit(:title, :description, :score, :airline_id)
--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,110 @@
-import React from 'react'
+import React, {useState, useEffect, Fragment} from 'react'
+import axios from 'axios'
+import Header from './Header'
+import ReviewForm from './ReviewForm'
+import styled from 'styled-components'
 
-const Airline = () =>{
-    return <div>This is the Airlines# show view for our app.</div>
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+`
+const Column = styled.div` 
+    background: #fff;
+    height: 100vh;
+    overflow: scroll;
+
+    &:last-child{
+        background: #000;
+    }
+`
+const Main = styled.div`
+    padding-left: 50px;
+`
+
+
+const Airline = (props) =>{
+    const [airline, setAirline] = useState({})
+    const [review, setReview] = useState({})
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(()=>{
+        const slug = props.match.params.slug
+        const url = `/api/v1/airlines/${slug}`
+        
+        axios.get(url)
+        .then (resp => {
+            setAirline(resp.data)
+            setLoaded(true)
+         })
+        .catch (resp => console.log(resp))
+    },[])
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        
+        setReview(Object.assign({}, review, {[e.target.name]: e.target.value}))
+
+        console.log('review:', review)
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const csrfToken = document.querySelector('[name=csrf-token]').content
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+        const airline_id = airline.data.id
+        axios.post('/api/v1/reviews', {review, airline_id})
+        .then(resp => {
+            const included = [...airline.include, resp.data.data]
+            setAirline({...airline, included})
+            setReview({title: '', description: '', score: 0})
+        })
+        .catch(resp => {
+
+        })
+        
+    }
+
+    const setRating = (score, e) => {
+        e.preventDefault()
+        
+        setReview({...review, score})
+        
+    }
+
+    return (
+    <Wrapper>
+        {
+        loaded &&
+        <Fragment>
+        <Column>
+            <Main>
+                
+                <Header
+                attributes ={airline.data.attributes}
+                reviews ={airline.included}
+                />
+                <div className="reviews"></div>
+            </Main>
+        </Column>
+        <Column>
+            <ReviewForm
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                setRating={setRating}
+                attributes={airline.data.attributes}
+                review={review}
+            />
+        </Column>
+        </Fragment>
+        }
+    </Wrapper>
+    )
 }
 
 export default Airline
