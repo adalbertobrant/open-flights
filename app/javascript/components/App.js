0b59df1da28bc031e870ae8dab75d3604b1e71fd
adalbertobrant <adalbertobrant@gmail.com>
2021-02-28 13:40:48 -0300

    adding font-awesome for stars

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Airlines from './Airlines/Airlines'
 import Airline from './Airline/Airline'
+import 'node_modules/font-awesome/css/font-awesome.css'
 
 const App =() =>{
 
